529411c24ac6cbd9f9d2481724b67b385c702de3
Mipsmonsta <yaojintham@gmail.com>
2025-09-13 10:26:01 +0800

    updated runserver.bat

--- runserver.bat
+++ runserver.bat
@@ -9,17 +9,4 @@ start "DjangoServer" /min cmd /k "python manage.py runserver"
 :: Open the site in the default browser
 start http://127.0.0.1:8000
 
-echo.
-echo Django server is running. Press any key to stop the server and close the browser...
-pause >nul
-
-:: Kill the Django server window by title
-taskkill /FI "WINDOWTITLE eq DjangoServer*" /T /F
-
-:: Close common browsers if opened
-taskkill /IM msedge.exe /F >nul 2>&1
-taskkill /IM chrome.exe /F >nul 2>&1
-taskkill /IM firefox.exe /F >nul 2>&1
-
-echo All processes stopped.
 pause
